214efa428cc631d55f3beda66ab1a2a59ddffc98
George Toothman <george.toothman@gmail.com>
2015-08-01 21:56:19 -0400

    add cartoon fixes

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+
+  dwarves.each_with_index do |name, index_count|
+    puts "#{index_count + 1}. #{name}"
+    index_count += 1
+  end
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  final_array = []
+  planeteer_calls.collect! do |call|
+    new_array = call.capitalize + "!"
+    final_array << new_array
+  end
+  final_array
 end
 
-def long_planteer_calls# code an argument here
+def long_planteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? do |length|
+    length.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
